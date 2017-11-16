2d56804b7bf2e57c23c5e75a4bf462d261bcf331
danny-hillebrand <danny-hillebrand@hotmail.com>
2017-11-16 12:01:54 +0100

    bootstrap includes
    bootstrap includes staan hier in

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="../htcbeachsoccer/bootstrap/css/bootstrap.min.css" >
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="../htcbeachsoccer/bootstrap/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="../htcbeachsoccer/bootstrap/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="../htcbeachsoccer/bootstrap/css/bootstrap-theme.min.css" >
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="../htcbeachsoccer/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<a class="btn btn-primary">???</a>
+</body>
+</html>
