26e3411a1dd68757110bcdc1a27a72c24106203d
Kevinh038 <kevinhendriks69@gmail.com>
2017-11-20 11:39:43 +0100

    Contactformulier
    Check ff

--- contact.php
+++ contact.php
@@ -1,10 +1,19 @@
 <?php 
 include 'header.php';
 ?>
-
-
-
-
+<form>
+<p>Naam: </p>
+<input name="Naam" type="text">
+<br>
+<p>Emailadres: </p>
+<input name="Emailadres" type="text">
+<br>
+<p>Bericht:</p>
+<br>
+<textarea name="Bericht" rows="10" cols="60"></textarea>
+  	<br>
+  	<input type="submit">
+</form>
 <?php 
 include 'footer.php';
 ?>
