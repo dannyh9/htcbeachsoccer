ac2e5e53087ad6f6ca187daf236040741aff905b
danny-hillebrand <danny-hillebrand@hotmail.com>
2017-11-23 13:39:44 +0100

    admin paneel
    admin

---
+++ admin/admin.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link href="../bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="./css/admin.css" rel="stylesheet">
+</head>
+<body>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+  <div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3 menu ">
+          <div class="nav-side-menu">
+              <div class="brand">Brand Logo</div>
+              <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+            
+                  <div class="menu-list">
+            
+                      <ul id="menu-content" class="menu-content collapse out">
+                          <li>
+                            <a href="#">
+                            <i class="fa fa-dashboard fa-lg"></i> Dashboard
+                            </a>
+                          </li>
+          
+                          <li  data-toggle="collapse" data-target="#products" class="collapsed active">
+                            <a href="#"><i class="fa fa-gift fa-lg"></i> UI Elements <span class="arrow"></span></a>
+                          </li>
+                          <ul class="sub-menu collapse" id="products">
+                              <li class="active"><a href="#">CSS3 Animation</a></li>
+                              <li><a href="#">General</a></li>
+                          </ul>
+          
+          
+                          <li data-toggle="collapse" data-target="#service" class="collapsed">
+                            <a href="#"><i class="fa fa-globe fa-lg"></i> Services <span class="arrow"></span></a>
+                          </li>  
+                          <ul class="sub-menu collapse" id="service">
+                            <li>New Service 1</li>
+                            <li>New Service 2</li>
+                            <li>New Service 3</li>
+                          </ul>
+          
+          
+                          <li data-toggle="collapse" data-target="#new" class="collapsed">
+                            <a href="#"><i class="fa fa-car fa-lg"></i> New <span class="arrow"></span></a>
+                          </li>
+                          <ul class="sub-menu collapse" id="new">
+                            <li>New New 1</li>
+                            <li>New New 2</li>
+                            <li>New New 3</li>
+                          </ul>
+          
+          
+                           <li>
+                            <a href="#">
+                            <i class="fa fa-user fa-lg"></i> Profile
+                            </a>
+                            </li>
+          
+                           <li>
+                            <a href="">
+                            <i class="fa fa-envelope-o fa-lg"></i> Verstuurde contact formulieren
+                            </a>
+                          </li>
+                      </ul>
+               </div>
+          </div>
+      </div>
+      <div class="col-sm-9 content">
+                          <h1>Welcome To Dashboard Panel :)</h1>
+                          <p>
+                              this  is test text.this  is test text.this  is test text.this  is test text.
+                              this  is test text.this  is test text.this  is test text.this  is test text.
+                              this  is test text.this  is test text.this  is test text.this  is test text.
+                              this  is test text.this  is test text.this  is test text.this  is test text.
+                              this  is test text.this  is test text.
+                          </p>
+                      </div>
+      </div>
+  </div>
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="./bootstrap/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="../bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="./js/script.js"></script> 
+</body>    
+</html>
