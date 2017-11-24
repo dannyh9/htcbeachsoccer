2593a3ee1263158f9a9440c8f1c37d71820e0aaa
Mhoogy <marcohoogland@msn.com>
2017-11-24 11:55:34 +0100

    ./
    Alles gefixt in admin./persoon/team/nieuwsartikel

--- admin/nieuwsartikel.php
+++ admin/nieuwsartikel.php
@@ -9,7 +9,7 @@
   });
   </script>
 <style>.bootstrap-iso .formden_header h2, .bootstrap-iso .formden_header p, .bootstrap-iso form{font-family: Arial, Helvetica, sans-serif; color: black}.bootstrap-iso form button, .bootstrap-iso form button:hover{color: white !important;} .asteriskField{color: red;}</style>
-<link rel="stylesheet" type="text/css" href="css/formulier.css">
+<link rel="stylesheet" type="text/css" href="../css/formulier.css">
 <script src="../js/uploadknopscript.js"></script>
 
 <div class="bootstrap-iso">
