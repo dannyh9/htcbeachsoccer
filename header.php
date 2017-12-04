c8532054808456addae172ba2874a9af179546b0
danny-hillebrand <danny-hillebrand@hotmail.com>
2017-12-04 15:29:28 +0100

    nieuwsartikelen overzicht en bekijken

--- header.php
+++ header.php
@@ -50,6 +50,9 @@
             <a class="nav-link" href="#">Agenda</a>
           </li>
           <li class="nav-item">
+            <a class="nav-link" href="nieuwsartikelen.php">Nieuwsartikelen</a>
+          </li>
+          <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
           </li>
           <li class="nav-item dropdown">
