8f29efd1c7f679cf83cc537b6016f91c59470412
Kevinh038 <kevinhendriks69@gmail.com>
2017-12-04 15:07:32 +0100

    Rightmenu + googlemaps
    Maps in contactformulier gezet

--- right-menu.php
+++ right-menu.php
@@ -1,14 +1,32 @@
 <div class="col-6 col-md-3 sidebar-offcanvas" id="sidebar">
+  <style>
+  .boldtext{
+    font-weight:bold;
+  }
+</style>
           <div class="list-group">
-            <div class="container text-center" style="border:1px solid #blue">
-              <strong> Zaterdag 9 december </strong>
+            <div class="container text-center boldtext" style="border:1px darkblue solid">
+              Zaterdag 9 december
               <br>
-              <strong> 12:00 uur </strong>
+              12:00 uur
               <br>
               <a> <img src="./img/svhtc.png" style="max-width:60px"></a>
+               - 
               <a> <img src="./img/svhtc.png" style="max-width:60px"></a>
+              <br>
+              SV HTC 1 - SV HTC 2
             </div>
-          </div>
+            <br><br>
+              <div class="container text-center boldtext" style="border:1px darkblue solid">
+              Zaterdag 9 december
+              <br>
+              12:00 uur
+              <br>
+              <a> <img src="./img/svhtc.png" style="max-width:60px"></a>
+               - 
+              <a> <img src="./img/svhtc.png" style="max-width:60px"></a>
+              <br>
+              SV HTC 1 - SV HTC 2
         </div><!--/span-->
       </div><!--/row-->
   <hr>
