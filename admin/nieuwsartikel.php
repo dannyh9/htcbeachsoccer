6e1e4854ebd9b2a4cdf4abf6e13cd5c432b34208
Mischahe <33717816+Mischahe@users.noreply.github.com>
2017-12-14 09:52:09 +0100

    commit

--- admin/nieuwsartikel.php
+++ admin/nieuwsartikel.php
@@ -2,14 +2,14 @@
 include '../databaseconnection.php';
 ?> 
 <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" /> 
- <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
- <script src="../tinymce/plugin/tinymce.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="../tinymce/plugin/tinymce.min.js"></script>
 <script>
   tinymce.init({
     selector: '.tinymce',
     language: 'nl'
   });
-  </script>
+</script>
 <style>.bootstrap-iso .formden_header h2, .bootstrap-iso .formden_header p, .bootstrap-iso form{font-family: Arial, Helvetica, sans-serif; color: black}.bootstrap-iso form button, .bootstrap-iso form button:hover{color: white !important;} .asteriskField{color: red;}</style>
 <link rel="stylesheet" type="text/css" href="../css/formulier.css">
 <script src="../js/uploadknopscript.js"></script>
@@ -21,16 +21,24 @@ function redirectoverview(){
 
 
 if(isset($_GET["nieuwsartikelid"])) {
-    $id = $_GET["nieuwsartikelid"];
-    $idquery = "SELECT * FROM nieuwsartikel WHERE ArtikelID = '$id'";
-    $result = $conn->query($idquery);
-    $row = mysqli_fetch_array($result);
-    $titel=$row['Titel'];
-    $inhoud=$row["Inhoud"];
+  $id = $_GET["nieuwsartikelid"];
+  $idquery = "SELECT * FROM nieuwsartikel WHERE ArtikelID = '$id'";
+  $result = $conn->query($idquery);
+  $row = mysqli_fetch_array($result);
+  $titel=$row['Titel'];
+  $inhoud=$row["Inhoud"];
+  $Afbeelding = $row["Afbeelding"];
+  var_dump($Afbeelding);
+  var_dump($id);
+  print_r($Afbeelding);
+  // var_dump($row);
 } else {
   $titel="";
   $inhoud="";
   $id="";
+  $Afbeelding = "";
+  var_dump($Afbeelding);
+  var_dump($id);
 }
 
 ?>
@@ -38,92 +46,101 @@ if(isset($_GET["nieuwsartikelid"])) {
  <div class="container-fluid">
   <div class="row">
    <div class="col-md-6 col-sm-6 col-xs-12">
-<form id="nieuwsartikel-form" method="POST" action="nieuwsartikel.php" role="form" enctype="multipart/form-data"> 
+    <form id="nieuwsartikel-form" method="POST" action="nieuwsartikel.php" role="form" enctype="multipart/form-data"> 
      <div class="form-group ">
        <form id="nieuwsartikel-form" method="post" action="index.php?nieuwsartikelid=<?php echo $id; ?>" role="form"> 
-      <label class="control-label " for="titel">
-       Titel
+        <label class="control-label " for="titel">
+         Titel
          <span class="asteriskField">
-        *
-      </label>
-      <input class="form-control" id="titel" name="titel" type="text" placeholder="Vul hier de titel in *" value="<?php echo $titel; ?>"/>
-     </div>
-     <div class="form-group ">
-      <label class="control-label " for="inhoud">
-       Inhoud
+          *
+        </label>
+        <input class="form-control" id="titel" name="titel" type="text" placeholder="Vul hier de titel in *" value="<?php echo $titel; ?>"/>
+      </div>
+      <div class="form-group ">
+        <label class="control-label " for="inhoud">
+         Inhoud
          <span class="asteriskField">
-        *
-      </label>
-      <textarea class="form-control tinymce" cols="40" id="inhoud" name="inhoud" rows="10""/> <?php echo $inhoud; ?> </textarea>
-     </div>
-     <div class="form-group">
-          <div class="form-group">
-        <label>
-        Upload foto
-      </label>
-        <div class="input-group">
+          *
+        </label>
+        <textarea class="form-control tinymce" cols="40" id="inhoud" name="inhoud" rows="10""/> <?php echo $inhoud; ?> </textarea>
+      </div>
+      <div class="form-group">
+        <div class="form-group">
+          <label>
+            Upload foto
+          </label>
+          <?php
+          if($Afbeelding != ""){
+            ?>
+            <div class="form-group ">
+              <img src="../uploads/<?php echo $Afbeelding; ?>">
+            </div>
+            <?php
+          }
+          ?>
+          <div class="input-group">
             <span class="input-group-btn">
-                <span class="btn btn-default btn-file">
-                    Zoeken… <input type="file" id="imgInp" name="file">
-                </span>
+              <span class="btn btn-default btn-file">
+                Zoeken… <input type="file" id="imgInp" name="file">
+              </span>
             </span>
             <input type="text" class="form-control" readonly>
-        </div>
-        <img id='img-upload'/>
+          </div>
+          <img id='img-upload'/>
           <div>
             <br>
-       <button class="btn btn-primary " name="submit" type="submit">
-        Submit
-       </button>
+            <button class="btn btn-primary " name="submit" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-    </form>
-   </div>
+    </div>
   </div>
- </div>
 </div>
 <?php 
 if (isset($_POST['submit'])){
 
-    $titel=$_POST['titel'];
-    $inhoud=$_POST['inhoud'];
-    $username="Admin";
+  $titel=$_POST['titel'];
+  $inhoud=$_POST['inhoud'];
+  $username="Admin";
 
-    $nieuwsartikelquery = "INSERT INTO `nieuwsartikel`(`Titel`, `Inhoud`, `Username`) VALUES ('$titel', '$inhoud', '$username')";
+  $nieuwsartikelquery = "INSERT INTO `nieuwsartikel`(`Titel`, `Inhoud`, `Username`) VALUES ('$titel', '$inhoud', '$username')";
   
     //$conn->query($nieuwsartikelquery);
 
-    $file = $_FILES['file'];
-    $fileName = $file['name'];
-    $fileTmpName = $file['tmp_name'];
-    $fileSize = $file['size'];
-    $fileError = $file['error'];
-    $fileType = $file['type'];
+  $file = $_FILES['file'];
+  $fileName = $file['name'];
+  $fileTmpName = $file['tmp_name'];
+  $fileSize = $file['size'];
+  $fileError = $file['error'];
+  $fileType = $file['type'];
   
-    $fileExt = explode('.', $fileName);
-    $fileActualExt = strtolower(end($fileExt));
+  $fileExt = explode('.', $fileName);
+  $fileActualExt = strtolower(end($fileExt));
   
-    $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+  $allowed = array('jpg', 'jpeg', 'png', 'pdf');
   
-    if (in_array($fileActualExt, $allowed)) {
-      if ($fileError === 0) {
-        if ($fileSize < 1000000) {
-          $fileNameNew = uniqid('', true).".".$fileActualExt;
-          $fileDestination = '../uploads/'.$fileNameNew;
-          move_uploaded_file($fileTmpName, $fileDestination);
-          $nieuwsartikelquery = "INSERT INTO `nieuwsartikel`(`Titel`, `Inhoud`, `Username`,`Afbeelding`) VALUES ('$titel', '$inhoud', '$username','$fileNameNew')";
+  if (in_array($fileActualExt, $allowed)) {
+    if ($fileError === 0) {
+      if ($fileSize < 1000000) {
+        $fileNameNew = uniqid('', true).".".$fileActualExt;
+        $fileDestination = '../uploads/'.$fileNameNew;
+        move_uploaded_file($fileTmpName, $fileDestination);
+        $nieuwsartikelquery = "INSERT INTO `nieuwsartikel`(`Titel`, `Inhoud`, `Username`,`Afbeelding`) VALUES ('$titel', '$inhoud', '$username','$fileNameNew')";
           //var_dump($fileNameNew);
           //header("Location: index.php?uploadsuccess");
-        } else {
-          //echo "Your file is too big!";
-        }
       } else {
-        //echo "There was an error uploading your file!";
+          //echo "Your file is too big!";
       }
     } else {
-      //echo "You cannot upload files of this type!";
+        //echo "There was an error uploading your file!";
     }
-    $conn->query($nieuwsartikelquery);
-    redirectoverview();
+  } else {
+      //echo "You cannot upload files of this type!";
+  }
+  $conn->query($nieuwsartikelquery);
+  redirectoverview();
 }
 
 ?>
