54d3660f6fe81dde0ffe28dc6ce469a8288a90bc
danny-hillebrand <danny-hillebrand@hotmail.com>
2017-12-21 13:10:29 +0100

    kevins ruk code maar weer eens fixe :D

--- right-menu.php
+++ right-menu.php
@@ -8,44 +8,45 @@ include('databaseconnection.php');
     font-weight:bold;
   }
 </style>
-              <?php
-              $date = new DateTime('yesterday');
-              $dateQuery = $date->format('Y-m-d H:i:s');
-              
-              $thuisteamquery="SELECT * FROM wedstrijd WHERE Datum >= '$dateQuery' LIMIT 2";
-              $result=mysqli_query($conn, $thuisteamquery);
-              $row=mysqli_fetch_array($result);
-              if($result){?>
-          <div class="container">
-            <div class="text-center boldtext" style="border:1px #164394 solid" >
-              <?php
-              $datum=$row['Datum'];
-              $datum=strtotime($datum);
-              $datum=date('d-m-Y H:i', $datum);
-              $tijd=substr($datum, -5);
-              $datumprint=substr($datum, 0, 10);
-              print($datumprint);
-              ?>
-              <br>
-              <?php
-              print($tijd);
-              ?>
-              <br>
-              <a> <img src="uploads/teamlogo/<?php if($row['Thuisteamlogo'] != NULL){echo($row['Thuisteamlogo']);} else{echo 'placeholder.png';}?>" style="max-width:60px"></a>
-               - 
-               <a> <img src="uploads/teamlogo/<?php if($row['Uitteamlogo'] != NULL){echo($row['Uitteamlogo']);} else{echo 'placeholder.png';}?>" style="max-width:60px"></a>
-              <br><?php
-              echo($row['Thuisteam']); echo(" - " . $row['Uitteam'] . "")?>
-            </div>
-            <br><br>
-            <hr>
-            <?php
-          }
-            ?>
-           <a class="twitter-timeline" data-lang="nl" data-width="320" data-height="420" data-theme="light" href="https://twitter.com/PBSZ?ref_src=twsrc%5Etfw">
-            Tweets by PBSZ
-          </a>
-          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div><!--/span-->
-      </div><!--/row-->
+<?php
+$date = new DateTime('yesterday');
+$dateQuery = $date->format('Y-m-d H:i:s');
+
+$thuisteamquery="SELECT * FROM wedstrijd WHERE Datum >= '$dateQuery' LIMIT 2";
+$result=mysqli_query($conn, $thuisteamquery);
+$row=mysqli_fetch_array($result);
+if ($result->num_rows === 0) {
+}else{?>
+<div class="container">
+  <div class="text-center boldtext" style="border:1px #164394 solid" >
+    <?php
+    $datum=$row['Datum'];
+    $datum=strtotime($datum);
+    $datum=date('d-m-Y H:i', $datum);
+    $tijd=substr($datum, -5);
+    $datumprint=substr($datum, 0, 10);
+    print($datumprint);
+    ?>
+    <br>
+    <?php
+    print($tijd);
+    ?>
+    <br>
+    <a> <img src="uploads/teamlogo/<?php if($row['Thuisteamlogo'] != NULL){echo($row['Thuisteamlogo']);} else{echo 'placeholder.png';}?>" style="max-width:60px"></a>
+    - 
+    <a> <img src="uploads/teamlogo/<?php if($row['Uitteamlogo'] != NULL){echo($row['Uitteamlogo']);} else{echo 'placeholder.png';}?>" style="max-width:60px"></a>
+    <br><?php
+    echo($row['Thuisteam']); echo(" - " . $row['Uitteam'] . "")?>
+  </div>
+  <br><br>
   <hr>
+  <?php
+}
+?>
+<a class="twitter-timeline" data-lang="nl" data-width="320" data-height="420" data-theme="light" href="https://twitter.com/PBSZ?ref_src=twsrc%5Etfw">
+  Tweets by PBSZ
+</a>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div><!--/span-->
+</div><!--/row-->
+<hr>
