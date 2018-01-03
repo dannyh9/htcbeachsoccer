f481e3e5da5ac7fdf06dcf7fdd8be3a15ed607a3
dannyh9 <danny-hillebrand@hotmail.com>
2018-01-03 21:03:11 +0100

    wedstrijd aanpassingen

--- admin/wedstrijd.php
+++ admin/wedstrijd.php
@@ -1,5 +1,45 @@
+<?php 
+    if(!isset($_SESSION['rollid'],$_SESSION['user']) && empty($_SESSION['rollid']) && empty($_SESSION['user'])) {
+        exit;
+    }
+?>
 <?php
 include '../databaseconnection.php';
+
+function redirectoverview(){
+  ?><script>window.location.replace("../admin/index.php?page=wedstrijdoverzicht");</script><?php
+}
+
+if(isset($_GET['wedstrijdid'])){
+  $id = mysqli_real_escape_string($conn,$_GET['wedstrijdid']);
+  $thuisteam="";
+  $thuisteamlogo="";
+  $uitteam="";
+  $uitteamlogo="";
+  $datum="";
+  $idquery = "SELECT * FROM wedstrijd WHERE WedstrijdID = '$id'";
+    $result = $conn->query($idquery);
+
+    //geen resultaat ga terug naar overzicht
+    if($result->num_rows === 0){ 
+      redirectoverview();
+      //
+    } else {
+      $row = mysqli_fetch_array($result);
+      $thuisteam=$row["Thuisteam"];
+      $thuisteamlogo=$row["Thuisteamlogo"];
+      $uitteam=$row["Uitteam"];
+      $uitteamlogo=$row["Uitteamlogo"];
+      $datum=$row["Datum"];
+    }
+} else {
+  $id="";
+  $thuisteam="";
+  $thuisteamlogo="";
+  $uitteam="";
+  $uitteamlogo="";
+  $datum="";
+}
 ?> 
 <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" /> 
 <link rel="stylesheet" type="text/css" href="../css/formulier.css">
@@ -12,7 +52,19 @@ include '../databaseconnection.php';
  <div class="container-fluid">
   <div class="row">
    <div class="col-md-6 col-sm-6 col-xs-6">
-    <form id="wedstrijd-form" method="POST" action="?page=wedstrijd" role="form" enctype="multipart/form-data"> 
+    <?php 
+      if(isset($_GET['wedstrijdid'])){ 
+          ?>
+             <form id="wedstrijd-form" method="POST" action="?wedstrijdid=<?php echo $_GET['wedstrijdid']; ?>" role="form" enctype="multipart/form-data"> 
+          <?php 
+      } else {
+        ?>
+          <form id="wedstrijd-form" method="POST" action="?page=wedstrijd" role="form" enctype="multipart/form-data"> 
+        <?php 
+      }
+    ?>
+   
+       
      <div class="form-group"> 
       <label class="control-label " for="thuisteam">
        Thuisteam
@@ -20,10 +72,18 @@ include '../databaseconnection.php';
         *
       </span>
       </label>
-      <input class="form-control" id="thuisteam" name="thuisteam" type="text" value=""/>
+      <input class="form-control" id="thuisteam" name="thuisteam" type="text" value="<?php echo $thuisteam;?>"/>
     </div>
     <div class="form-group"> 
       <label class="control-label " for="thuisteam">Teamlogo thuisteam</label>
+      <?php 
+        if($thuisteamlogo !="") {
+          ?>
+          <br>
+          <img width="150" src="../uploads/teamlogo/<?php echo $thuisteamlogo; ?>">
+          <?php
+        }
+      ?>
       <div class="input-group">
         <span class="input-group-btn">
           <span class="btn btn-default btn-file">
@@ -40,10 +100,18 @@ include '../databaseconnection.php';
           *
           </span>
         </label>
-        <input class="form-control" id="uitteam" name="uitteam" type="text" placeholder="Vul hier de titel in *"/>
+        <input class="form-control" id="uitteam" name="uitteam" type="text" value="<?php echo $uitteam;?>" placeholder="Vul hier de titel in *"/>
       </div>
       <div class="form-group"> 
       <label class="control-label " for="thuisteam">Teamlogo uitteam</label>
+      <?php 
+        if($uitteamlogo !="") {
+          ?>
+          <br>
+          <img width="150" src="../uploads/teamlogo/<?php echo $uitteamlogo; ?>">
+          <?php
+        }
+      ?>
       <div class="input-group">
         <span class="input-group-btn">
           <span class="btn btn-default btn-file">
@@ -53,16 +121,22 @@ include '../databaseconnection.php';
         <input type="text" class="form-control" readonly>
       </div>
     </div>
-        
-        <div class="form-group ">
+     <div class="form-group ">
+      <?php 
+      if($datum =="" ) {
+      ?>
+       
           <label class="control-label" for="datum">
             Datum wedstrijd (Datum en tijd):
           </label>
           <span class="asteriskField">
             *
-            <input type="datetime-local" id="datum" name="datum">
-          </div>
-          <div class="messages" style="font-size:20px"></div>
+            <input type="datetime-local" id="datum"  name="datum">
+          
+          
+      <?php } ?>
+      </div>
+      <div class="messages" style="font-size:20px"></div>
           <button class="btn btn-primary " name="submit" type="submit">
             Opslaan
           </button>
@@ -84,14 +158,23 @@ if(isset($_POST['submit'])){
             </script>
     <?php
     } else {
-      $id = "";
+      //$id = "";
       $thuisteam=mysqli_escape_string($conn, $_POST['thuisteam']);
       $uitteam=mysqli_escape_string($conn, $_POST['uitteam']);
-      $datum=mysqli_escape_string($conn, $_POST['datum']);
-      $datum=str_replace("T", " ", $datum);
-      $datum=str_replace('/', '-', $datum);
-      $datum=strtotime($datum);
-      $datumquery=date('Y-m-d H:i', $datum);
+      if($id !=""){
+        if(isset($_POST['datum'])){
+        $datum=mysqli_escape_string($conn, $_POST['datum']);
+        $datum=str_replace("T", " ", $datum);
+        $datum=str_replace('/', '-', $datum);
+        $datum=strtotime($datum);
+        $datumquery=date('Y-m-d H:i', $datum);
+        } else {
+           $datumquery="2888-08-07 14:00:00";
+        }
+        
+      } else {
+        $datumquery="2888-08-07 14:00:00";
+      }
       $datumhuidig = new DateTime();
       $datumhuidig = $datumhuidig->format('Y-m-d H:i');
       //var_dump($datumhuidig , $datumquery);
@@ -101,9 +184,17 @@ if(isset($_POST['submit'])){
 
        if(!file_exists($_FILES['logothuis']['tmp_name']) || !is_uploaded_file($_FILES['logothuis']['tmp_name']) && !file_exists($_FILES['logouit']['tmp_name']) || !is_uploaded_file($_FILES['logouit']['tmp_name'] )) {
 
-            $wedstrijdquery="INSERT INTO `wedstrijd`(`Datum`, `Thuisteam`, `Uitteam`) VALUES ('$datumquery', '$thuisteam', '$uitteam')";
+            if(isset($_GET['wedstrijdid'])){ 
+                $wedstrijdquery="UPDATE wedstrijd SET Thuisteam = '$thuisteam', Uitteam = '$uitteam' WHERE WedstrijdID = $id ";
+            }
+            else {
+                 $wedstrijdquery="INSERT INTO `wedstrijd`(`Datum`, `Thuisteam`, `Uitteam`) VALUES ('$datumquery', '$thuisteam', '$uitteam')";
+            }
+
+            
             //var_dump($_FILES);
             $conn->query($wedstrijdquery);
+            redirectoverview();
           ?>
           <script>$(".messages").text("De ingevulde wedstrijd is toegevoegd.");</script>
           <?php
@@ -166,8 +257,14 @@ if(isset($_POST['submit'])){
               echo "You cannot upload files of this type!";
             }
             if(!empty($thuislogonaam) && !empty($uitlogonaam)){
-              $wedstrijdquery = "INSERT INTO `wedstrijd`(`Datum`, `Thuisteam`, `Uitteam`,`Thuisteamlogo`, `Uitteamlogo`) VALUES ('$datumquery', '$thuisteam', '$uitteam' , '$thuislogonaam' , '$uitlogonaam')";
+              if(isset($_GET['wedstrijdid'])){ 
+              $wedstrijdquery="UPDATE `wedstrijd` SET `Datum`='$datumquery', `Thuisteam` = '$thuisteam', `Uitteam` = '$uitteam',`Thuisteamlogo` = '$thuislogonaam',`Thuisteamlogo` = '$uitlogonaam' WHERE WedstrijdID = '$id'"; 
+              }else {
+                $wedstrijdquery = "INSERT INTO `wedstrijd`(`Datum`, `Thuisteam`, `Uitteam`,`Thuisteamlogo`, `Uitteamlogo`) VALUES ('$datumquery', '$thuisteam', '$uitteam' , '$thuislogonaam' , '$uitlogonaam')";
+              }
               $conn->query($wedstrijdquery);
+
+              redirectoverview();
               ?><script>$(".messages").text("De ingevulde wedstrijd is toegevoegd.");</script><?php
             } else {
               ?><script>$(".messages").text("Er is iets fout gegaan probeer het opnieuw!");</script><?php
